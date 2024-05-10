04f01b8352b97569321297d344f189aba6e15f94
hiranj-kotak <95754714+hiranj-kotak@users.noreply.github.com>
2024-05-10 10:44:00 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
                 <>
                   <NavDropdown title={userInfo.name} id='username'>
                     <LinkContainer to='/profile'>
-                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                      <NavDropdown.Item>profile2</NavDropdown.Item>
                     </LinkContainer>
                     <NavDropdown.Item onClick={logoutHandler}>
                       Logout
